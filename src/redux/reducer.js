1b0b7283cba477fc3d4f84b236a799df198a3ff7
Віталій Гоцуляк <shuricks007@gmail.com>
2022-11-29 00:42:56 +0200

    changed redux to redux toolkit

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
+import { addTask, deleteTask, setStatusFilter, toggleCompleted } from "./actions";
+import { createReducer } from "@reduxjs/toolkit";
 import { statusFilters } from "./constants";
-import { combineReducers } from "redux";
 
 const tasksInitialState = [
   { id: 0, text: "Learn HTML and CSS", completed: true },
@@ -13,39 +14,29 @@ const filtersInitialState = {
   status: statusFilters.all,
 };
 
-const tasksReducer = (state = tasksInitialState, action) => {
-  switch (action.type) {
-    case "tasks/addTask":
-      return [...state, action.payload];
-    case "tasks/deleteTask":
-      return state.filter(task => task.id !== action.payload);
-    case "tasks/toggleCompleted":
-      return state.map(task => {
-        if (task.id !== action.payload) return task;
+export const tasksReducer = createReducer(tasksInitialState, {
+  [addTask]: (state, action) => {
+    return [...state, action.payload];
+  },
+  [deleteTask]: (state, action) => {
+    return state.filter(task => task.id !== action.payload);
+  },
+  [toggleCompleted]: (state, action) => {
+    return state.map(task => {
+      if(task.id !== action.payload){
+        return task;
+      }
+      return {...task,
+      completed: !task.completed,}
+    })
+  },
+});
 
-        return {
-          ...task,
-          completed: !task.completed,
-        };
-      });
-    default:
-      return state;
-  }
-};
-
-const filtersReducer = (state = filtersInitialState, action) => {
-  switch (action.type) {
-    case "filters/setStatusFilter":
-      return {
-        ...state,
-        status: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-export const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    filters: filtersReducer,
-  });
+export const filtersReducer = createReducer(filtersInitialState, {
+  [setStatusFilter]: (state, action) => {
+    return {
+      ...state,
+      status: action.payload,
+    }
+  },
+});
