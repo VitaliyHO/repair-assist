d933a55dbefb94395b2e9e5052614904fb9c7dbe
Віталій Гоцуляк <shuricks007@gmail.com>
2023-02-15 01:53:25 +0200

    deleted old code

--- src/redux/tasks/slice.js
+++ src/redux/tasks/slice.js
@@ -17,11 +17,6 @@ export const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
     items: [
-      //   { id: 0, text: "Learn HTML and CSS", completed: true },
-      //   { id: 1, text: "Get good at JavaScript", completed: true },
-      //   { id: 2, text: "Master React", completed: false },
-      //   { id: 3, text: "Discover Redux", completed: false },
-      //   { id: 4, text: "Build amazing apps", completed: false },
     ],
     isLoading: false,
     error: null,
@@ -60,35 +55,6 @@ export const tasksSlice = createSlice({
         state.items[taskIndex].completed = action.payload.completed;
       })
       .addCase(updateTask.rejected, handleRejected),
-
-  //   reducers: {
-  //     addTask: {
-  //       reducer(state, action) {
-  //         state.items.push(action.payload);
-  //       },
-  //       prepare(text) {
-  //         return {
-  //           payload: {
-  //             text,
-  //             id: nanoid(),
-  //             completed: false,
-  //           },
-  //         };
-  //       },
-  //     },
-  //     deleteTask(state, action) {
-  //       const index = state.items.findIndex((task) => task.id === action.payload);
-  //       state.items.splice(index, 1);
-  //     },
-  //     toggleCompleted(state, action) {
-  //       for (const task of state.items) {
-  //         if (task.id === action.payload) {
-  //           task.completed = !task.completed;
-  //           break;
-  //         }
-  //       }
-  //     },
-  //   },
 });
 
 // const persistConfig = {
